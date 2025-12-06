296c0ebab23af2b9cc72e39fe88b388cce99854a
veniapputri <veniapputri@gmail.com>
2025-12-06 11:00:05 +0700

    hasn't been finished yet

--- RGeneral/simple_moving_averages.R
+++ RGeneral/simple_moving_averages.R
@@ -5,9 +5,24 @@ library(languageserver)
 install.packages("readr")  
 library(readr)
 
+#task : 
+#You work in finance and one of your clients wants to understand 
+#the value of different company stocks over time. 
+#Given a dataset of stock prices, you decide to use simple moving averages 
+#(window length = 20) to tackle this task. 
+#What companies have an upward trend for the most recent data? 
+#And what companies have a downward trend?
+
 #load data
 stock <- read_csv("data/stock_details_5_years.csv")
-View(stock)
+
+#Descriptive statistics
+#Ich will das Datum verändern, Date-Typ zum konvertieren
+stock$Date <- gsub(x=stock$Date, pattern="T.*", replacement="")
+stock$Date <- as.Date(stock$Date)
+
+
+
 
 
 
