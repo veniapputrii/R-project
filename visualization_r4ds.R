e927dc150ea15df56f0e6aae3a489779d9315222
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-07-27 13:51:04 +0700

    Add files via upload

--- visualization_r4ds.R
+++ visualization_r4ds.R
@@ -7,10 +7,16 @@ penguins
 glimpse(penguins)
 ggplot(
   data = penguins,
-  mapping = aes(x = flipper_length_mm, y = body_mass)
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
   ) +
-  geom_point()+
-  geom_smoooth(method = 'lm')
+  geom_point(aes(color = species, shape = species))+
+  geom_smooth(method = 'lm') +
+  labs(
+    title = "Body mass and flipper length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gento penguins",
+    x = "Flipper length (mm)", y = "Body mass (g)",
+    
+  ) 
 
 
 library(tidyverse)
