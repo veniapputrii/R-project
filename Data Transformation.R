98192f6cdd31bd0dc7656dae1a6bf2cf237c36e1
Veni Agustina <61000887+veniapputrii@users.noreply.github.com>
2026-09-17 09:24:11 +0200

    Remove tibble output comments from Data Transformation.R
    Removed commented output of tibble structure from the script.

--- Data Transformation.R
+++ Data Transformation.R
@@ -115,14 +115,3 @@ flights |>
     gain = dep_delay - arr_delay,
     speed = distance / air_time * 60
   )
-#> # A tibble: 336,776 × 21
-#>    year month   day dep_time sched_dep_time dep_delay arr_time sched_arr_time
-#>   <int> <int> <int>    <int>          <int>     <dbl>    <int>          <int>
-#> 1  2013     1     1      517            515         2      830            819
-#> 2  2013     1     1      533            529         4      850            830
-#> 3  2013     1     1      542            540         2      923            850
-#> 4  2013     1     1      544            545        -1     1004           1022
-#> 5  2013     1     1      554            600        -6      812            837
-#> 6  2013     1     1      554            558        -4      740            728
-#> # ℹ 336,770 more rows
-#> # ℹ 13 more variables: arr_delay <dbl>, carrier <chr>, flight <int>, …
